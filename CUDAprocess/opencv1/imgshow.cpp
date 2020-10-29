81ce311abb63026c0eb1ee73b0892f7cbf32ed6a
laishixuan <laishixuan123@gmail.com>
2020-10-29 20:06:50 +0800

    添加新的项目，用于作业

--- CUDAprocess/opencv1/imgshow.cpp
+++ CUDAprocess/opencv1/imgshow.cpp
@@ -1,6 +1,7 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
 #include <math.h>
+#include <string.h>
 
 using namespace std;
 using namespace cv;
@@ -9,22 +10,46 @@ void readimg();
 void creatandchange();
 void at_pixel();
 void Buer();
+void accuracy_check();
+void black_check();
 
-int main(int, char *argv[])
-
+int main(void)
 {
-	Mat image = imread("D://Dropbox//earth2.jpg");
-	imshow("cLena.png", image);
-	Rect rect2(10, 20, 100, 200);	//区域从（10，20）开始取点，宽100，高200像素
-	Rect rect(10, 10, 150, 100);   //区域的左上角点的坐标为（10,10）
-								   //区域宽为150，高为100
-	Mat image_ori = image(rect2);
-	namedWindow("结果", 1);
-	imshow("结果", image_ori);
-	waitKey();
-	return 0;
+	readimg();
+	//int i = 1;
 
+	//for (i = 1; i <= 15; i++) {
+	//	Mat image = imread("d://大型工作文件//08 葡萄面积处理//处理后图像//" + to_string(i) + "-1.jpg", IMREAD_UNCHANGED);
+	//	long img_area[3] = { 0 };
+
+	//	//遍历图片所有点，并获取原始深度，并输出坐标
+	//	for (int row = 0; row < image.rows; row++)
+	//	{
+	//		for (int col = 0; col < image.cols; col++)
+	//		{
+	//			if (image.at<uchar>(row, col) > 0) {
+	//				if ((row > -1) && (row < 160)) {
+	//					img_area[0]++;
+	//				}
+	//				else if ((row > 159) && (row < 320)) {
+	//					img_area[1]++;
+	//				}
+	//				else if ((row > 319) && (row < 480)) {
+	//					img_area[2]++;
+	//				}
+	//			}
+	//		}
+
+	//	}
+
+	//	cout << img_area[0] << '\t' << img_area[1] << '\t' << img_area[2] << endl;
+	//}
+
+	cin.get();
+	return 0;
 }
+
+	
 
 //读取一张位于D盘的图片，并生成灰白照片，再保存
 void readimg() {
@@ -117,4 +142,74 @@ void Buer() {
 			if (pow((i - img1.rows / 2), 2) + pow((j - img1.cols / 2), 2) >= pow(150, 2))
 				r.at<uchar>(i, j) = white;
 	imshow("Result1", r);
+}
+
+void accuracy_check(){
+	Mat src = imread("d://0914test//2-3//org_3.jpg", IMREAD_UNCHANGED);
+	//imshow("abc", src);
+	uchar pixel1 = src.at<uchar>(0, 0);
+	int num = 0;
+	int sumdepth = 0;
+	for (int i = 316; i < 447; i = i + 10) {
+		for (int j = 0; j < 597; j = j + 10) {
+			pixel1 = src.at<uchar>(i, j);
+			if (pixel1 > 255)
+				continue;
+			else {
+				num++;
+				sumdepth = pixel1 * 12 + sumdepth;
+			}
+		}
+	}
+
+	cout << (double)sumdepth / (double)num << endl;
+}
+
+void black_check() {
+	int i = 1;
+	int count[3] = { 0 };
+
+	for (i = 1; i <= 15; i++) {
+		Mat src = imread("d://大型工作文件//08 葡萄面积处理//" + to_string(i) + ".png", IMREAD_UNCHANGED);
+		//Mat src = imread("d://大型工作文件//08 葡萄面积处理//left_data.frame-1 - 副本.png", IMREAD_UNCHANGED);
+		count[0] = 0;
+		count[1] = 0;
+		count[2] = 0;
+
+
+		for (int i = 0; i < 160; i++) {
+			for (int j = 0; j < src.cols; j++) {
+				Vec4b & pixel = src.at<Vec4b>(i, j);
+				if ((int)pixel[3] != 255) {
+					continue;
+				}
+				else {
+					count[0]++;
+				}
+			}
+		}
+		for (int i = 160; i < 320; i++) {
+			for (int j = 0; j < src.cols; j++) {
+				Vec4b & pixel = src.at<Vec4b>(i, j);
+				if (pixel[3] != 255) {
+					continue;
+				}
+				else {
+					count[1]++;
+				}
+			}
+		}
+		for (int i = 320; i < 480; i++) {
+			for (int j = 0; j < src.cols; j++) {
+				Vec4b & pixel = src.at<Vec4b>(i, j);
+				if (pixel[3] != 255) {
+					continue;
+				}
+				else {
+					count[2]++;
+				}
+			}
+		}
+		cout << count[0] << '\t' << count[1] << '\t' << count[2] << endl;
+	}
 }
---
+++ CUDAprocess/opencv_hw/main.cpp
@@ -0,0 +1,7 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <math.h>
+#include <string.h>
+
+using namespace std;
+using namespace cv;
